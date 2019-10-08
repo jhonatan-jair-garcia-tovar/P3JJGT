ba2b63afd975b8dcfb9f4b3c3d77defb4542742d
JHONATAN JAIR GARCIA TOVAR <56013281+jhonatan-jair-garcia-tovar@users.noreply.github.com>
2019-10-07 20:02:37 -0500

    Initial commit

---
+++ app/src/main/java/com/mx/edu/isc/p3jjgt/Frm2activity.java
@@ -0,0 +1,14 @@
+package com.mx.edu.isc.p3jjgt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Frm2activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm2activity);
+    }
+}
